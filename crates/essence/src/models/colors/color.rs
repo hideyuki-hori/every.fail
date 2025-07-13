cca01ecc46eace8ab79feee57d3584ae2e2879ef
- <montecarlo1101@gmail.com>
2025-07-13 21:01:04 +0900

    さすがに意味なさすぎtestだったので修正

--- crates/essence/src/models/colors/color.rs
+++ crates/essence/src/models/colors/color.rs
@@ -39,12 +39,17 @@ mod tests {
     #[test]
     fn rgb() {
         let r = Color::rgb(Value::full(), Value::zero(), Value::zero());
-        assert_eq!(r, Color::rgb(Value::full(), Value::zero(), Value::zero()));
+        assert_eq!(r.r, Value::full());
+        assert_eq!(r.g, Value::zero());
+        assert_eq!(r.b, Value::zero());
     }
 
     #[test]
     fn rgba() {
         let r = Color::rgba(Value::full(), Value::zero(), Value::zero(), Value::full());
-        assert_eq!(r, Color::rgba(Value::full(), Value::zero(), Value::zero(), Value::full()));
+        assert_eq!(r.r, Value::full());
+        assert_eq!(r.g, Value::zero());
+        assert_eq!(r.b, Value::zero());
+        assert_eq!(r.a, Value::full());
     }
 }
